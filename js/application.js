64dc032331c60d45b070fbc2a9e968fb4d6d0b82
teelu <terrence.lun@gmail.com>
2015-07-11 22:53:47 +0800

    Completed basic cup object with functions for moving object around

--- js/application.js
+++ js/application.js
@@ -1,3 +1,60 @@
 $(document).ready(function() {
-	console.log("READY");
+	//Array for keeping moves
+	var movesArray = [];
+
+
+	//cup functions
+	function cup(name,position,hasBall) {
+		this.name = name;
+		this.position = position;
+		this.hasBall = hasBall;
+		this.getKeyFromValue = function(value) {
+			for(var keys in this) {
+				if (this[keys]==value) {
+					return this;
+				}
+			}
+		}
+	}
+
+	function moveLeft(position) {
+		return position--;
+	}
+
+	function moveRight(position) {
+		return position++;
+	}
+
+
+	function move(position,direction) {
+
+	}
+	//cup holder(har har har)
+	var cupArray = [];
+
+	//generate cups
+	for (var i = 1; i <= 3; i++) {
+		cupArray.push(new cup("cup" + i,i,false));
+		console.log(cupArray);
+	}
+	console.log(cupArray[0].name + " " + cupArray[0].hasBall);
+
+	 //Pick a cup
+	 console.log("Where to insert ball");
+	 // var cupSelected = prompt();
+	 // //set cup selected
+	 // cupArray[cupSelected-1].hasBall = true;
+
+	 console.log(cupArray);
+
+	 $('html').keypress(function(e) {
+	 	console.log(String.fromCharCode(e.keyCode));
+	 });
+	 console.log(cupArray[1].getKeyFromValue(2));
+
+
+
+
+
+
 });
